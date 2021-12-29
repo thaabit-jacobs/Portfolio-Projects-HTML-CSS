a9bba281a6bc55ca652f7d8fd63d21b67824b1f3
thaabit <mohammedthaabitjacobs@gmail.com>
2021-12-29 17:15:59 +0200

    table

--- asynchronous prgramming/promises.js
+++ asynchronous prgramming/promises.js
@@ -1,6 +1,33 @@
-function getPeopleData(){
-    return fetch("/data.json")
-    .then(data => data);
+let  people = {
+    "thaabit": {    "name": "thaabit",
+                    "age": 23
+                },
+
+    "kauthar": {
+        "name": "kauthar",
+        "age": 22
+    },
+
+    "peter" :   {
+        "name": "Peter",
+        "age": 34
+    }         
+
 }
 
-console.log(getPeopleData());
+
+function getPerson(name){
+    if(people.hasOwnProperty(name))
+        return Promise.resolve(people[name]);
+    return Promise.resolve(new Error("Name does not exist"));       
+}
+
+let person = getPerson("lee")
+                .then(p => {
+                    p["surname"] = "Jacobs";
+                    return Promise.resolve(p);
+                })  
+                .then(p2 => console.log(p2))
+                .catch(err => console.log(err))
+      
+let timeOut = 
