ed53e89305567435c0881459b6a4530b853936f0
thaabit <mohammedthaabitjacobs@gmail.com>
2021-12-30 20:08:59 +0200

    flappy bird

---
+++ flappy bird/script.js
@@ -0,0 +1,16 @@
+/*
+const element = document.getElementById('some-element-you-want-to-animate');
+
+console.log(element.style.top)
+console.log(element.getBoundingClientRect())
+
+
+function step(timestamp) {
+    if (Number(element.style.top.replace("px", "")) !== v) { 
+        element.style.top = Number(element.style.top.replace("px", "")) + 2 + "px";
+        window.requestAnimationFrame(step);
+    }
+}
+
+window.requestAnimationFrame(step);
+*/
