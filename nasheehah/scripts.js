8357fb5fcb763895f4122e9ed6567fb600d97f4e
thaabit <mohammedthaabitjacobs@gmail.com>
2022-01-06 11:45:26 +0200

    layer4 render data completed

--- nasheehah/scripts.js
+++ nasheehah/scripts.js
@@ -17,107 +17,128 @@ function getTimesBasedOnUserCoordinates(position){
     let latitude = position.coords.latitude;
     let longitude = position.coords.longitude;
 
-    let prayTimes = getPrayTimes(latitude, longitude, currentDateData());
-    prayTimes.then(data => console.log(data.data));
+    saveLatAndLongToLocalStorage(latitude, longitude);
 }
 
 //if do do not have access to the users location via geo location
-//retrive it using a pop up form
+//use default data
 //once retrieved data will be saved in localstorage for subsequent request
 function getTimesBasedOnUserCity() {
         if(!localStorage.latitude || !localStorage.longitude){
-            createCityForm();
-
-            const cityForm = document.querySelector("#city-form");
-    
-            cityForm.addEventListener("submit", (event) => {
-            event.preventDefault();
-    
-            const selectedCity = cityForm.elements["city-name"].value;
-    
-            let city = getCity(selectedCity)
-            .then(data => {
-                localStorage.setItem("latitude", data[0]["latitude"]);
-                localStorage.setItem("longitude", data[0]["longitude"]);
-                localStorage.setItem("city", data[0]["name"]);
-
-                let prayTimes = getPrayTimes(localStorage.latitude, localStorage.longitude, currentDateData());
-                prayTimes.then(data => console.log(data.data));
-
-                document.querySelector(".modal").remove();
-            })
-            .catch(error => {
-                addErrorMsgToInput("Please enter valid city name")
-            });
-    })
-        }else{
-            let prayTimes = getPrayTimes(localStorage.latitude, localStorage.longitude, currentDateData());
-            prayTimes.then(data => console.log(data.data));
+            saveLatAndLongToLocalStorage(-26.0025, 28.0244);
         }
 }
 
-function getPrayTimes(latitude, longitude, date){
-    return fetch(`http://api.aladhan.com/v1/calendar?latitude=${latitude}&longitude=${longitude}&method=3&month=${date[1]}&year=${date[0]}`)
-            .then(response => response.json())
-            .catch(error => console.log(error));     
+function saveLatAndLongToLocalStorage(latitude, longitude){
+    localStorage.longitude = longitude;
+    localStorage.latitude = latitude;
+
+    return [latitude, longitude];
 }
 
-function getCity(selectedCity){
-    return fetch(`https://api.api-ninjas.com/v1/city?name=${selectedCity}`, {
-        headers: {
-            'Content-Type': 'application/json',
-            'X-Api-Key': 'pR64g0X81XnhAEuJ5zA0nA==GhjyE9fmWRMOmkDd'
+async function getPrayTimesCurrentMonthForYear(latitude, longitude, date){
+    let [year, month] = date();
+
+    let url = `http://api.aladhan.com/v1/calendar?latitude=${latitude}&longitude=${longitude}&method=3&month=${month}&year=${year}`;
+    
+    try{
+        let response = await fetch(url);
+        let results = await response.json();
+        return results.data;
+    }catch(error) {
+        console.log(error);
+    }
+}
+
+
+async function renderPrayDataForToday(){
+    let praysForCurrentMonth = await getPrayTimesCurrentMonthForYear(localStorage.latitude, localStorage.longitude, currentDateData);
+    let currentDate = new Date();
+
+    praysForCurrentMonth.forEach(prayTime => {
+        if(areDatesEqual(currentDate, extractDate(prayTime.date.gregorian.date))){
+            console.log(prayTime);
+
+            renderDates(prayTime.date.hijri,  prayTime.date.gregorian);
+
+            renderPrayTimes(prayTime.timings);
+
+            renderMetaData(prayTime.meta);
         }
     })
-    .then(response => response.json())
 }
 
-function createCityForm(){
-    let formDiv = document.createElement("div");
-    formDiv.className = "modal";
-
-    let form = document.createElement("form");
-    form.setAttribute("id", "city-form");
-
-    formDiv.appendChild(form);
-    
-    let cityInput = document.createElement("input");
-    cityInput.setAttribute("id", "city-name");
-    cityInput.setAttribute("name", "city-name");
-    cityInput.setAttribute("type", "text");
-    cityInput.setAttribute("placeholder", "Enter city name i.e Cape Town");
-    
-    let submitInput = document.createElement("input");
-    submitInput.className = "contact-form"
-    submitInput.setAttribute("id", "submit-btn");
-    submitInput.setAttribute("name", "submit-btn");
-    submitInput.setAttribute("type", "submit");
-
-    form.appendChild(cityInput);
-    form.appendChild(submitInput);
-
-    document.body.appendChild(formDiv)
+function coOrdinateFormater(lat, long){
+    return `${lat}, ${long}`;
 }
 
-function addErrorMsgToInput(errorMsg){
-    let currentError = document.querySelector(".error");
+function paramsFormatter(paramsObj){
+    return `Fajr ${paramsObj.Fajr}.0 degrees, Isha ${paramsObj.Isha}.0 degrees`
+}
 
-    if(currentError)  currentError.remove();
+function prayNameFormatter(prayName){
+    prayName = prayName.toLowerCase();
+    prayName = prayName.substring(0, 1).toUpperCase() + prayName.substring(1); 
+    return prayName;
+}
 
-    let newError = document.createElement("p");
-    newError.className = "error";
-    newError.innerText = errorMsg;
+function renderDates(hirijiDateObj, gregorianDateObj){
+    const hirijiDateEl = document.querySelector("#hijiri-date");
+    hirijiDateEl.innerText = dateBuilder(hirijiDateObj.day, hirijiDateObj.month.en, hirijiDateObj.year);
 
-    document.querySelector("#city-form").appendChild(newError);
+    let gregorianDateEl = document.querySelector("#gregorian-date");
+    gregorianDateEl.innerText = dateBuilder(gregorianDateObj.day, gregorianDateObj.month.en, gregorianDateObj.year);
+}
+
+function renderPrayTimes(prayTimings){
+    document.querySelectorAll(".pray").forEach(pray => {
+        let currentPray = prayNameFormatter(pray.querySelector(".pray-name").innerText);
+        pray.querySelector(".pray-time").innerText = prayTimings[currentPray]; 
+    })
+}
+
+function renderMetaData(metaObj){
+    let methodEl = document.querySelector("#method");
+    methodEl.innerText = metaObj.method.name;
+
+    let paramsEl = document.querySelector("#params");
+    paramsEl.innerText = paramsFormatter(metaObj.method.params);
+
+    let schoolEl = document.querySelector("#school");
+    schoolEl.innerText = metaObj.school;
+
+    let coOrdinatesEl = document.querySelector("#co-ordinates");
+    coOrdinatesEl.innerText = coOrdinateFormater(metaObj.latitude, metaObj.longitude);
+}
+
+function dateBuilder(day, month, year){
+    return `${day} ${month}, ${year}`;
+}
+
+function areDatesEqual(date1, date2){
+    return (date1.getFullYear() === date2.getFullYear()) && (date1.getMonth() === date2.getMonth()) &&
+     (date1.getDate() === date2.getDate());
 }
 
 function currentDateData(){
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
     let currentDate = new Date();
     let currentYear = currentDate.getFullYear();
     let currentMonth = currentDate.getMonth() + 1;
+    let currentDay = days[currentDate.getDay()];
 
-    return [currentYear, currentMonth]
+    return [currentYear, currentMonth, currentDay, currentDate.getDay()];
+}
+
+function extractDate(strDate){
+    let day = Number(strDate.substring(0, strDate.indexOf("-")));
+    let month = Number(strDate.substring(strDate.indexOf("-") + 1, strDate.lastIndexOf("-")));
+    let year = Number(strDate.substring(strDate.lastIndexOf("-") + 1));
+
+    return new Date(year, --month, day);
 }
 
 getLocation();
 
+renderPrayDataForToday();
