8a591e3332bd9f1c8d486c5398fa8476123b6062
thaabit <mohammedthaabitjacobs@gmail.com>
2022-01-10 08:01:10 +0200

    calendar table

--- nasheehah/scripts.js
+++ nasheehah/scripts.js
@@ -55,9 +55,12 @@ async function renderPrayDataForToday(){
     let praysForCurrentMonth = await getPrayTimesCurrentMonthForYear(localStorage.latitude, localStorage.longitude, currentDateData);
     let currentDate = new Date();
 
+    console.log(praysForCurrentMonth);
+
+    RenderPrayForMonth(praysForCurrentMonth)
+
     praysForCurrentMonth.forEach(prayTime => {
         if(areDatesEqual(currentDate, extractDate(prayTime.date.gregorian.date))){
-            console.log(prayTime);
 
             renderUpcomingPray(prayTime.timings);
 
@@ -69,6 +72,102 @@ async function renderPrayDataForToday(){
         }
     })
 }
+
+
+function RenderPrayForMonth(prayDataForMonth){
+    const hijriCalendar = document.querySelector("#hijri-calendar");
+
+    let trHeadings = document.createElement("tr");
+
+    let th1 = document.createElement("th");
+    th1.className = "cell";
+    th1.innerText = "Gregorian Date";
+
+    let th2 = document.createElement("th");
+    th2.className = "cell";
+    th2.innerText = "Hijri Date";
+
+    let th3 = document.createElement("th");
+    th3.className = "cell";
+    th3.innerText = "Fajr";
+    
+    let th4 = document.createElement("th");
+    th4.className = "cell";
+    th4.innerText = "Sunrise";
+
+    let th5 = document.createElement("th");
+    th5.className = "cell";
+    th5.innerText = "Dhuhr";
+
+    let th6 = document.createElement("th");
+    th6.className = "cell";
+    th6.innerText = "Asr";
+
+    let th7 = document.createElement("th");
+    th7.className = "cell";
+    th7.innerText = "Magrib";
+
+    let th8 = document.createElement("th");
+    th8.className = "cell";
+    th8.innerText = "Isha";
+
+    trHeadings.appendChild(th1);
+    trHeadings.appendChild(th2);
+    trHeadings.appendChild(th3);
+    trHeadings.appendChild(th4);
+    trHeadings.appendChild(th5);
+    trHeadings.appendChild(th6);
+    trHeadings.appendChild(th7);
+    trHeadings.appendChild(th8);
+
+    hijriCalendar.appendChild(trHeadings);
+
+    prayDataForMonth.forEach(prayData => {
+        let tr = document.createElement("tr");
+        
+        let td1 = document.createElement("td");
+        td1.innerText = prayData.date.gregorian.date;
+
+        let td2 = document.createElement("td");
+        td2.innerText = prayData.date.hijri.date;
+
+        let td3 = document.createElement("td");
+        td3.className = "bg-white font-weight-none";
+        td3.innerText = prayData.timings.Fajr;
+
+        let td4 = document.createElement("td");
+        td4.className = "bg-white font-weight-none";
+        td4.innerText = prayData.timings.Sunrise;
+
+        let td5 = document.createElement("td");
+        td5.className = "bg-white font-weight-none";
+        td5.innerText = prayData.timings.Dhuhr;
+
+        let td6 = document.createElement("td");
+        td6.className = "bg-white font-weight-none";
+        td6.innerText = prayData.timings.Asr;
+
+        let td7 = document.createElement("td");
+        td7.className = "bg-white font-weight-none";
+        td7.innerText = prayData.timings.Maghrib;
+
+        let td8 = document.createElement("td");
+        td8.className = "bg-white font-weight-none";
+        td8.innerText = prayData.timings.Isha;
+
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+        tr.appendChild(td3);
+        tr.appendChild(td4);
+        tr.appendChild(td5);
+        tr.appendChild(td6);
+        tr.appendChild(td7);
+        tr.appendChild(td8);
+
+        hijriCalendar.appendChild(tr);
+    })
+}
+
 
 function currentDateData(){
     const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
@@ -83,8 +182,8 @@ function currentDateData(){
 
 function nextPray(timings){
     let currentDateHours = Number(new Date().getHours());
-    let currentDateMinutes = Number(new Date().getMinutes());
-    let lowestPrayHour = 100;
+    let currentDateMinutes = Number(new Date().getMinutes()) + currentDateHours * 60;
+    let lowestPrayHour = 1000;
     let prayTimeResult = "";
     let prayNameResult = "";
 
@@ -92,18 +191,14 @@ function nextPray(timings){
         if(!(prayName === "Sunset" || prayName === "Imsak" || prayName === "Midnight")){
             let currentPrayTime = timings[prayName];
             let formattedPrayHour = getPrayTimeHoursAndMins(currentPrayTime)[0];
-            let formattedPrayMin = getPrayTimeHoursAndMins(currentPrayTime)[1];
-
-            if(formattedPrayHour > currentDateHours){
+            let formattedPrayMin = getPrayTimeHoursAndMins(currentPrayTime)[1] + formattedPrayHour * 60;
+            
+            if(formattedPrayMin > currentDateMinutes){
                 if(formattedPrayHour < lowestPrayHour){
                     lowestPrayHour = formattedPrayHour;
                     prayTimeResult = currentPrayTime;
                     prayNameResult = prayName; 
                 }
-            }
-
-            if(formattedPrayHour === currentDateHours){
-                
             }
         }
     }
@@ -196,6 +291,59 @@ function extractDate(strDate){
     return new Date(year, --month, day);
 }
 
-getLocation();
+if(localStorage.latitude !== undefined && localStorage.longitude !== undefined){
+    getLocation();
+}
 
 renderPrayDataForToday();
+
+const form = document.querySelector("#date-picker-form");
+
+form.addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    let dateInfo = form["month-year-picker"].value;
+
+    let praysForCurrentMonth = await getPrayTimesCurrentMonthForYearInfo(localStorage.latitude, localStorage.longitude, extractDateInfo(dateInfo));
+
+    const hijriCalendar = document.querySelector("#hijri-calendar");
+    [...hijriCalendar .childNodes].forEach(children => children.remove())
+    //console.log(praysForCurrentMonth);
+
+    RenderPrayForMonth(praysForCurrentMonth);
+})
+
+function extractDateInfo(date){
+    return [date.substring(0, date.indexOf("-")), date.substring(date.indexOf("-") + 1)];
+}
+
+async function getPrayTimesCurrentMonthForYearInfo(latitude, longitude, date){
+    
+    let url = `http://api.aladhan.com/v1/calendar?latitude=${latitude}&longitude=${longitude}&method=3&month=${date[1]}&year=${date[0]}`;
+    
+    try{
+        let response = await fetch(url);
+        let results = await response.json();
+        return results.data;
+    }catch(error) {
+        console.log(error);
+    }
+}
+
+let isHidden = false;
+
+const showButton = document.querySelector("#show-button");
+showButton.addEventListener("click", (event) => {
+    const table = document.querySelector("table");
+
+    if(!isHidden){
+        table.className = "bg-off-white hide";
+        showButton.innerText = "show";
+        isHidden = true;
+    }else{
+        table.className = "bg-off-white";
+        showButton.innerText = "hide";
+        isHidden = false;
+    }
+
+})
