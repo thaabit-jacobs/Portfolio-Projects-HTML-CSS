90395462649e7fbeb944c4fe45343b5c41705a49
thaabit <mohammedthaabitjacobs@gmail.com>
2022-01-12 14:24:58 +0200

    completed undewater

--- Underwater landing page/scripts.js
+++ Underwater landing page/scripts.js
@@ -1,5 +1,13 @@
 const menue = document.querySelector("#menue");
+const overlay = document.querySelector(".over-lay");
+const closeBtn = document.querySelector("#close");
 
 menue.addEventListener("click", () => {
-    
+    overlay.className = "over-lay"
+    overlay.style.diplay = "flex";
+    console.log(overlay.style.diplay)
+})
+
+closeBtn.addEventListener("click", () => {
+    overlay.className = "over-lay hide"  
 })
