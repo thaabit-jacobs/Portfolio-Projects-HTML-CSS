2c186b031a19f207156d489a7df339b788253b14
thaabit <mohammedthaabitjacobs@gmail.com>
2022-01-12 07:45:16 +0200

    started layout

---
+++ quiz app/script.js
@@ -0,0 +1,42 @@
+let questions = {
+    "What does HTML stand for"?: 
+    {
+        "Hyper Text preprocessor": false,
+        "Hyper Text Markup Language": true,
+        "Hyper Text Mutiple Language": false,
+        "Hyper Tool Multi Language": false,
+    },
+
+    "What does CSS stand for"?: 
+    {
+        "Common Style Sheet": false,
+        "Colorful Style Sheet": false,
+        "Computer Style Sheet": false,
+        "Cascading Style Sheet": true,
+    },
+
+    "What does PHP stand for"?: 
+    {
+        "Hypertext Preproccessor": true,
+        "Hypertext Programming": false,
+        "Hypertext Preprogramming": false,
+        "Hometext Preprocessor": false,
+    },
+
+    "What does SQL stand for"?: 
+    {
+        "Stylish Question Language": false,
+        "Stylish Query Language": false,
+        "Statement Question Language": false,
+        "Structured Query Language": true,
+    },
+
+    "What does XML stand for"?: 
+    {
+        "eXtensible Markup Language": true,
+        "eXcutable Mutiple Language": false,
+        "eXTra Mutiple-Program Language": false,
+        "eXamine Mutiple Language": false,
+    }
+}
+
