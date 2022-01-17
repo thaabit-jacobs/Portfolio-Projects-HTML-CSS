88b691bdbeeb9e4e886965f9ec86bf14c399bf2e
thaabit <mohammedthaabitjacobs@gmail.com>
2022-01-17 20:18:53 +0200

    favs

--- recipe app/script.js
+++ recipe app/script.js
@@ -54,6 +54,7 @@ async function getNRandomMeal(n){
         const resData = await response.json();
         const randomMeal = resData.meals[0];
 
+        console.log(randomMeal);
         renderMealCard(randomMeal)
     }
 }
@@ -66,15 +67,157 @@ function renderMealCard(mealObj){
     mealImg.className = "meal-card-img";
     mealImg.setAttribute("src", mealObj.strMealThumb);
 
-    let mealTitle = document.createElement("a");
-    mealTitle.className = "meal-card-name";
-    mealTitle.setAttribute("href", mealObj.strSource);
+    let mealTitle = createBtnEventListener(mealObj);
     mealTitle.innerText = mealObj.strMeal;
+
+    let mealHeart = createFavEventLisstener(mealObj);
 
     mealCard.appendChild(mealImg);
     mealCard.appendChild(mealTitle);
+    mealCard.appendChild(mealHeart);
 
     mealsContainer.appendChild(mealCard);
+}
+
+function createFavEventLisstener(mealObj){
+    let mealHeart = document.createElement("img");
+    mealHeart.className = "fav-icon";
+    mealHeart.setAttribute("src", "images/white-heart.svg");
+
+    mealHeart.addEventListener("click", () => {
+        if(mealHeart.getAttribute("src") === "images/white-heart.svg"){
+            mealHeart.setAttribute("src", "images/red-heart.svg");
+            addToFavourites(mealObj.idMeal);
+        }else{
+            mealHeart.setAttribute("src", "images/white-heart.svg");
+            removeFromFavourites(mealObj.idMeal);
+            console.log("hello")
+        }
+    })
+
+    return mealHeart;
+}
+
+//favourites
+
+function addToFavourites(mealId){
+    if(localStorage.favs === undefined){
+        let favs = "";
+        favs = favs.concat(mealId);
+        localStorage.favs = favs;
+    }else{
+        favs = localStorage.favs;
+        favs = favs.concat("," + mealId);
+        localStorage.favs = favs;
+    }
+
+    console.log(localStorage.favs)
+}
+
+function removeFromFavourites(mealId){
+    let favs = localStorage.favs;
+    let startMealIdIndex = favs.indexOf(mealId); 
+    let endMealIdIndex = favs.indexOf(",", startMealIdIndex); 
+
+    if(startMealIdIndex === 0){
+        favs = favs.substring(endMealIdIndex + 1);
+    }else if(endMealIdIndex === favs.length - 1){
+        favs = favs.substring(0, startMealIdIndex);
+    }else {
+        favs = favs.substring(0, startMealIdIndex).concat(favs.substring(endMealIdIndex + 1));
+    }
+
+    localStorage.favs = favs;
+
+    console.log(localStorage.favs)
+}
+
+//
+function createBtnEventListener(mealObj){
+
+    let btn = document.createElement("button");
+    btn.className = "meal-card-name";
+
+    btn.addEventListener("click", (event) => {
+        let overyLayCheck = document.querySelector(".over-lay");
+
+        if(overyLayCheck !== null){
+            console.log("We in !!!!!!!!!!");
+            overyLayCheck.remove();
+        }
+
+        let overLay = document.createElement("div");
+        overLay.className = "over-lay";
+
+        let recipeContainer = document.createElement("div");
+        recipeContainer.className = "recipe-container";
+
+        //<button class="my-2">X</button>
+        let closeBtn = document.createElement("button");
+        closeBtn.innerText = "X"
+        closeBtn.className = "my-2";
+
+        closeBtn.addEventListener("click", (event) => {
+            console.log(event)
+            const overLay = document.querySelector(".over-lay");
+            overLay.style.display = "none";
+        })
+
+        let recipeTitle = document.createElement("h2");
+        recipeTitle.className = "recipe-title";
+        recipeTitle.innerText = mealObj.strMeal;
+
+        let recipeImg = document.createElement("img");
+        recipeImg.className = "recipe-img";
+        recipeImg.setAttribute("src", mealObj.strMealThumb);
+
+        let recipeIngr = document.createElement("h3");
+        recipeIngr.className = "left-align mx-2 recipe-ingre";
+        recipeIngr.innerText = "Ingredients";
+
+        let ul = createIngredientList(mealObj);
+
+        let instructionsIngr = document.createElement("h3");
+        instructionsIngr.className = "left-align mx-2 recipe-instr";
+        instructionsIngr.innerText = "Instructions";
+
+        let instructions = document.createElement("p");
+        instructions.className = "left-align mx-2 my-1";
+        instructions.innerText = mealObj.strInstructions;
+
+        recipeContainer.appendChild(closeBtn);
+        recipeContainer.appendChild(recipeTitle);
+        recipeContainer.appendChild(recipeImg);
+        recipeContainer.appendChild(recipeIngr);
+        recipeContainer.appendChild(ul);
+        recipeContainer.appendChild(instructionsIngr);
+        recipeContainer.appendChild(instructions);
+
+        overLay.appendChild(recipeContainer);
+
+        document.querySelector("body").prepend(overLay);
+    })
+
+    return btn;
+}
+
+function createIngredientList(mealObject){
+    let ul = document.createElement("ul");
+    ul.className = "left-align mx-2 my-1 recipe-ingre-list";
+
+    let keys = Object.keys(mealObject);
+
+    console.log(keys);
+
+    keys.forEach(key => {
+        if(key.indexOf("strIngredient") !== -1 && mealObject[key] !== ""){
+            let li = document.createElement("li");
+            li.innerText = mealObject[key];
+            ul.appendChild(li);
+        }
+    })
+
+    return ul;
 }
 
 async function getAllMealsForCategory(category){
@@ -88,7 +231,7 @@ async function getAllMealsForCategory(category){
     renderMealCardCategory(meals);
 }
 
-async function getMealsById(id, mealTitle){
+async function getMealsById(id){
     const response = await fetch(
         `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
     );
@@ -96,9 +239,10 @@ async function getMealsById(id, mealTitle){
     const restData = await response.json();
     const meal = await restData.meals[0];
 
-    console.log(meal.strSource)
+    let mealTitle = createBtnEventListener(meal);
+    mealTitle.innerText = meal.strMeal;
 
-    mealTitle.setAttribute("href", meal.strSource);
+    return mealTitle;
 }
 
 function renderMealCardCategory(meals){
@@ -106,7 +250,7 @@ function renderMealCardCategory(meals){
     mealsContainer.className = "meals-container my-2";
     mealsContainer.id = "ingre-recipe";
 
-    meals.forEach(mealObj => {
+    meals.forEach(async mealObj => {
         //console.log(mealObj)
 
         let mealCard = document.createElement("div");
@@ -116,13 +260,9 @@ function renderMealCardCategory(meals){
         mealImg.className = "meal-card-img";
         mealImg.setAttribute("src", mealObj.strMealThumb);
     
-        let mealTitle = document.createElement("a");
-        mealTitle.className = "meal-card-name";
-        mealTitle.innerText = mealObj.strMeal;
-        mealTitle.setAttribute("target", "_blank");
-
-        getMealsById(mealObj.idMeal, mealTitle)
+        let mealTitle = await getMealsById(mealObj.idMeal);
     
+        console.log(mealTitle);
         mealCard.appendChild(mealImg);
         mealCard.appendChild(mealTitle);
     
@@ -139,6 +279,8 @@ function deleteIngredientRecipeContainer(){
         recipeIngredientCon.remove();
     }
 }
+
+
 chickenBtn.addEventListener("click", (event) => {
     deleteIngredientRecipeContainer()
     getAllMealsForCategory("chicken")
@@ -162,15 +304,6 @@ async function getMealByName(mealName){
     const restData = await response.json();
     const meal = await restData.meal;
 }   
-
-async function getMealsByMainIngredient(ingrediant){
-    const response = await fetch(
-        `https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingrediant}`
-    );
-
-    const restData = await response.json();
-    const meals = await restData.meals;
-}
 
 form.addEventListener("submit", async (event) => {
     event.preventDefault();
@@ -197,9 +330,7 @@ form.addEventListener("submit", async (event) => {
         mealImg.className = "meal-card-img";
         mealImg.setAttribute("src", mealObj.strMealThumb);
     
-        let mealTitle = document.createElement("a");
-        mealTitle.className = "meal-card-name";
-        mealTitle.setAttribute("href", mealObj.strSource);
+        let mealTitle = createBtnEventListener(mealObj);
         mealTitle.innerText = mealObj.strMeal;
     
         mealCard.appendChild(mealImg);
@@ -214,6 +345,7 @@ form.addEventListener("submit", async (event) => {
 
     form.reset();
 });
+
 
 getNRandomMeal(6)
 
