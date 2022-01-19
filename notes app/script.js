383d784dadbafa5fb435085286e90e898ff74c7f
thaabit <mohammedthaabitjacobs@gmail.com>
2022-01-19 20:34:21 +0200

    npte app view

---
+++ notes app/script.js
@@ -0,0 +1,20 @@
+/*let keysPressed = {};
+
+document.addEventListener('keydown', (event) => {
+keysPressed[event.key] = true;
+
+if (keysPressed['Control'] && event.key == 'a') {
+    alert(event.key);
+}
+});
+
+document.addEventListener('keyup', (event) => {
+    delete keysPressed[event.key];
+});
+
+const btn = document.querySelector("#click"); 
+
+btn.addEventListener("dblclick", (event) => {
+    console.log("hello world")
+})
+*/
