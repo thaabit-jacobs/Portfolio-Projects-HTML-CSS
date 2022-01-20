e7e8f37317bcc30ae3935e11a4d49a768cf1db78
thaabit <mohammedthaabitjacobs@gmail.com>
2022-01-20 20:33:07 +0200

    jjjj

--- notes app/script.js
+++ notes app/script.js
@@ -18,3 +18,38 @@ btn.addEventListener("dblclick", (event) => {
     console.log("hello world")
 })
 */
+
+let keysPressed = {};
+let displayHead = document.querySelector("input");
+let displayText = document.querySelector(".note-display-container-text");
+
+window.addEventListener("keydown", (event) => {
+    keysPressed[event.key] = true;
+    
+    if (keysPressed['Alt'] && event.key == 's') {
+        console.log(displayHead.value);
+        console.log(displayText.value);
+    }
+})
+
+window.addEventListener('keyup', (event) => {
+    delete keysPressed[event.key];
+});
+
+textarea = document.querySelector("textarea");
+
+/*
+textarea.addEventListener('input', autoResize, false);
+
+function autoResize() {
+    this.style.height = 'auto';
+    this.style.height = this.scrollHeight + 'px';
+}
+
+let notes = document.querySelectorAll(".note");
+notes.forEach(note => {
+    note.addEventListener("click", (event) => {
+
+    })
+})
+*/
