6e38592cb366ee4a64bc42b7809c6624f39977d9
thaabit <mohammedthaabitjacobs@gmail.com>
2022-01-21 21:02:17 +0200

    sort notes

--- notes app/script.js
+++ notes app/script.js
@@ -19,9 +19,28 @@ btn.addEventListener("dblclick", (event) => {
 })
 */
 
+let keysPressed = {};
+const displayHead = document.querySelector(".container-heading");
+const displayText = document.querySelector(".container-text");
+const textarea = document.querySelector("textarea");
+const addBtn = document.querySelector("#add-note");
+const notesContainer = document.querySelector(".all-notes-container-narrow");
+
+///////////////add note listener ///////////////
+addBtn.addEventListener("click", (event) => {
+    displayHead.value = "New note";
+    displayText.value = "A new note";
+
+    setActiveNoteId("");
+
+    let changeBgNote = document.querySelector(".note-gray-background");
+    if(changeBgNote !== null){
+        changeBgNote.className = "note";
+    }
+});
+
 
 ///////////////textearea///////////////
-textarea = document.querySelector("textarea");
 textarea.addEventListener('input', autoResize, false);
 
 function autoResize() {
@@ -30,18 +49,23 @@ function autoResize() {
 }
 
 ///////////////save heading and text///////////////
-let keysPressed = {};
-let displayHead = document.querySelector(".container-heading");
-let displayText = document.querySelector(".container-text");
-
-displayHead.value = "A Sample Note";
 
 window.addEventListener("keydown", (event) => {
     keysPressed[event.key] = true;
     
     if (keysPressed['Alt'] && (event.key == 's' || event.key == 'S')) {
-        console.log(displayHead.value);
-        console.log(displayText.value);
+        let note = {
+            heading: displayHead.value,
+            text: displayText.value,
+            date: formatDate(new Date()),
+            timestamp: new Date(),
+            id: getNextId()
+        }
+
+        displayHead.value = "";
+        displayText.value = "";
+        
+        saveNote(note);
     }
 })
 
@@ -49,10 +73,145 @@ window.addEventListener('keyup', (event) => {
     delete keysPressed[event.key];
 });
 
-///////////////selected note event listener ///////////////
-let notes = document.querySelectorAll(".note");
-notes.forEach(note => {
-    note.addEventListener("click", (event) => {
+///////////////localstorage DB ///////////////
 
+localStorage.idSequence = "0";
+localStorage.activeId = "";
+
+function getNextId(){
+    let currenId = localStorage.idSequence;
+    currenId = Number(currenId);
+    localStorage.idSequence = "" + ++currenId;
+    return currenId;
+}
+
+function setActiveNoteId(id){
+    localStorage.activeId = id;
+}
+
+function saveNote(note){    
+    if(localStorage.notes === undefined){
+        localStorage.notes = "" + JSON.stringify(note);
+    }else if(localStorage.notes === ""){
+        localStorage.notes = "" + JSON.stringify(note);
+    }else{
+        let currentActiveId = localStorage.activeId;
+
+        if(currentActiveId === ""){
+            localStorage.notes += "splitIndicator" + JSON.stringify(note);
+        }else{
+            let foundNote = localStorage.notes.split("splitIndicator")
+                                              .map(n => JSON.parse(n))
+                                              .find(n => n.id === Number(currentActiveId));
+
+                                              deleteNote(foundNote);
+            foundNote.heading = note.heading;                                    
+            foundNote.text = note.text;                                    
+            foundNote.date = formatDate(new Date());
+            
+            localStorage.notes += "splitIndicator" + JSON.stringify(foundNote);
+        }
+    }
+    renderNotes();
+}
+
+function deleteNote(note){
+    if(localStorage.notes === 1){
+        localStorage.notes = "";
+    }else {
+        let notes = localStorage.notes;
+        notes = notes.split("splitIndicator")
+                     .filter(savedNote => savedNote !== JSON.stringify(note))
+                     .join("splitIndicator");
+
+        localStorage.notes = notes;
+    }
+
+    renderNotes();
+}
+
+///////////////utilities ///////////////
+function renderNotes(){
+    let notes = localStorage.notes;
+
+    if(notes !== undefined || notes !== ""){
+        [...notesContainer.childNodes]
+                      .filter(child => child.id !== "add-note")
+                      .forEach(child => child.remove());
+
+        notes.split("splitIndicator")
+             .map(note => JSON.parse(note))
+             .sort((a, b) => new Date(b.timestamp) - new Date(a.timestamp))
+             .forEach(note => {
+                 console.log(note);
+                notesContainer.appendChild(createNote(note));
+             })
+    }else{
+        localStorage.notes = "";
+    }
+}
+
+function createNote(note){
+    let noteDiv = document.createElement("div");
+    noteDiv.className = "note";
+
+    let h2 = document.createElement("h2");
+    h2 = note["heading"];
+    
+    let textContent = document.createElement("p");
+    textContent.className = "note-text";
+    textContent.innerText = note["text"];
+
+    let textDate = document.createElement("p");
+    textDate.className = "note-date";
+    textDate.innerText = note["date"]
+
+    noteDiv.append(h2);
+    noteDiv.appendChild(textContent);
+    noteDiv.appendChild(textDate);
+
+    ///////////////delete note event listener ///////////////
+    noteDiv.addEventListener("dblclick", (event) => {
+        alert("You about to delete note");
+        deleteNote(note);
     })
-})
+
+    ///////////////selected note event listener ///////////////
+    noteDiv.addEventListener("click", (event) => {
+        let changeBgNote = document.querySelector(".note-gray-background");
+        if(changeBgNote !== null){
+            changeBgNote.className = "note";
+        }
+        
+        noteDiv.className = "note note-gray-background"
+        displayHead.value = note.heading;
+        displayText.value = note.text;  
+        
+        setActiveNoteId(note.id);
+    })
+
+    return noteDiv;
+}
+
+function formatDate(date){
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+    let currentDay = days[date.getDay()];
+    let currentDate = date.getDate();
+    let currentMonth = months[date.getMonth()];
+    let currentYear = date.getFullYear();
+    let currentHour = date.getHours();
+    let currentMin = date.getMinutes();
+    let currentSeconds = date.getSeconds();
+
+
+    return `${currentDay}, ${currentDate} ${currentMonth} ${currentYear} at ${currentHour}:${currentMin}:${currentSeconds}`;
+}
+
+function parseDate(dateStr){
+    
+}
+
+//localStorage.clear()
+renderNotes();
