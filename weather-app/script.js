dddcdb47dc1cf24036d1d686cdf5f7efcff3e3c5
thaabit <mohammedthaabitjacobs@gmail.com>
2022-01-25 17:52:12 +0200

    layout complete

--- weather-app/script.js
+++ weather-app/script.js
@@ -1,6 +1,8 @@
 const apiKey = "cf580544875b4f54906162956222401";
 const baseUrl = "http://api.weatherapi.com/v1";
 
+/*
 fetch("http://api.weatherapi.com/v1/current.json?key="+apiKey+"&q=Cape Town")
 .then(response => response.json())
 .then(data => console.log(data))
+*/
