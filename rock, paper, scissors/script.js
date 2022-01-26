6d6c0b8bd86426a5df1c06489e81ab33e4a18b08
thaabit <mohammedthaabitjacobs@gmail.com>
2022-01-26 20:45:58 +0200

    rock, paper, scissors

---
+++ rock, paper, scissors/script.js
@@ -0,0 +1,25 @@
+const playerScore = document.querySelector("#player-score");
+const computerScore = document.querySelector("#computer-score");
+
+const rockBtn = document.querySelector("#rock");
+const paperBtn = document.querySelector("#paper");
+const scissorsBtn = document.querySelector("#scissor");
+
+const winner = document.querySelector("#winner");
+
+let moves = ["rock", "paper", "scissors"];
+
+function round(playerMove, computerMove){
+    
+    if(playerMove === "rock" && computerMove === "scissors"){
+
+    }
+    
+    if(playerMove === "rock" && computerMove === "paper"){
+
+    }
+    
+    if(playerMove === "rock" && computerMove === "rock"){
+
+    }
+}
