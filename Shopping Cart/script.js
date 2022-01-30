77dad79fffad211bd2fe4b47d23f6995c47a6c81
thaabit <mohammedthaabitjacobs@gmail.com>
2022-01-30 09:58:41 +0200

    event listener

--- Shopping Cart/script.js
+++ Shopping Cart/script.js
@@ -1,5 +1,11 @@
 const productContainer = document.querySelector("#productContainer");
 
+let cart = {
+    products: [],
+    itemCount: 0,
+    totalPrice: 0
+}
+
 fetch('https://fakestoreapi.com/products')
             .then(res=>res.json())
             .then(json=> {
@@ -34,14 +40,19 @@ fetch('https://fakestoreapi.com/products')
 
                     let label = document.createElement("div");
                     label.className = "label";
-                    label.innerHTML = `
+                    label.innerHTML = 
+                    `
                     <svg width="16px" height="16px" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-cart-fill">
                         <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
                     </svg>
+                    <p>add to cart</p>
+                    `
 
-                    <p>add to cart</p>`
+                    label.addEventListener("click", () => {
+                        
+                    })
 
                     productDiv.appendChild(label);
                 });
-console.log(json)
 })
+
